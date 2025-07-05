7b8d24330ad69036dbb21bc391b6d2f059a5d24a
a.roman.rodriguez1@gmail.com <a.roman.rodriguez1@gmail.com>
2025-07-05 19:46:14 +0200

    [fix] add import in locale path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,3 +1,5 @@
+import path from 'path'
+
 const i18nextConfig = {
   i18n: {
     defaultLocale: 'es',
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import i18nextConfig from 'next-i18next.config'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  i18n: i18nextConfig.i18n,
+  i18n: i18nextConfig,
 }
 
 export default nextConfig
